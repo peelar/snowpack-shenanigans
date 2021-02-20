5a394418c5ad2f7eac9896e81192b30516f00c1c
Adrian Pilarczyk <adrian@useo.pl>
2021-02-20 19:41:49 +0100

    add: chakra-ui config

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,29 @@
+import {
+  extendTheme,
+  ChakraProvider,
+  ThemeConfig,
+  ColorModeScript
+} from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: true
+} as ThemeConfig;
+const theme = extendTheme({ config });
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  <>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <React.StrictMode>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  </>,
+  document.getElementById('root')
 );
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
